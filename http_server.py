702ae4abdfd0d64a04a271b8d68b876f858b4ba3
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2026-05-17 20:40:12 +0200

    Change HTTP server port from 8000 to 80
    Signed-off-by: Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>

--- http_server.py
+++ http_server.py
@@ -141,4 +141,4 @@ def set_temperature(name: str, body: TemperatureRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
